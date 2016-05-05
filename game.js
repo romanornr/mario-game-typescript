657502f5c8c48fd84c59bc52822112e2538fbba1
romanornr <romanornr@gmail.com>
2016-05-05 17:08:18 +0200

    minor adjustments

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ var w = 720, h = 480;
 var Mario = (function () {
     function Mario(x, y) {
         var _this = this;
+        this.downForce = 3;
         this.drawImage = function (ctx) {
             ctx.save();
             ctx.beginPath();
@@ -15,16 +16,16 @@ var Mario = (function () {
 }());
 var marioImage = new Image();
 marioImage.src = "graphics/mario/small/Standing-mario.gif";
+var mario1 = new Mario(70, 45);
 function gameLoop(ctx) {
     requestAnimationFrame(gameLoop);
     ctx.fillStyle = "rgb(174,238,238)";
     ctx.fillRect(0, 0, w, h);
     ctx.fillStyle = "rgb(14,253,1)";
-    ctx.fillRect(0, h - 45, w, 45);
+    var floor = ctx.fillRect(0, h - 45, w, 45);
     mario1.drawImage(ctx);
     //ctx.clearRect(0, 0, w, h);
 }
-var mario1 = new Mario(70, 45);
 window.onload = function () {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
--- game.ts
+++ game.ts
@@ -1,17 +1,18 @@
 var w = 720, h = 480;
 
 interface Character{
+    draw(): void;
     velocity: number;
-    heigth: number;
+    downForce: number;
     x: number;
     y: number;
-    src: any;
 }
 
 class Mario {
     public x: number;
     public y: number;
-    public src: any;
+    public downForce: number = 3;
+  
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
@@ -28,17 +29,18 @@ class Mario {
 var marioImage = new Image();
 marioImage.src = "graphics/mario/small/Standing-mario.gif";
 
+var mario1: Mario = new Mario(70, 45);
+
 function gameLoop(ctx){
     requestAnimationFrame(gameLoop);
     ctx.fillStyle = "rgb(174,238,238)";
     ctx.fillRect(0, 0, w, h);
     ctx.fillStyle = "rgb(14,253,1)";
-    ctx.fillRect(0, h - 45, w, 45);
+    var floor = ctx.fillRect(0, h - 45, w, 45);
     mario1.drawImage(ctx);
     //ctx.clearRect(0, 0, w, h);
 }
 
-var mario1: Mario = new Mario(70, 45)
 
 window.onload = () => {
     var canvas = <HTMLCanvasElement>document.getElementById("canvas");
