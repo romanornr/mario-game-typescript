e9f151bfbc976d8fb76a9b43edf72f75b5b7d221
romanornr <romanornr@gmail.com>
2016-05-05 09:37:26 +0200

    added graphics

--- game.js
+++ game.js
@@ -1,4 +1,17 @@
 var w = 512, h = 384;
+var Mario = (function () {
+    function Mario(velocity, heigth) {
+        this.velocity = velocity;
+        this.heigth = heigth;
+        this.discription = velocity + " this is is height: " + heigth;
+    }
+    return Mario;
+}());
+function intro(mario) {
+    return "This is Mario and " + mario.velocity + " " + mario.heigth;
+}
+var player = new Mario(1, 1.50);
+console.log(intro(player));
 function gameLoop(ctx) {
     requestAnimationFrame(gameLoop);
     ctx.fillStyle = "rgb(174,238,238)";
--- game.ts
+++ game.ts
@@ -1,5 +1,26 @@
 var w = 512, h = 384;
 
+interface Character{
+    discription: string;
+    velocity: number;
+    heigth: number;
+}
+
+class Mario {
+    discription: string;
+    constructor(public velocity, public heigth){
+        this.discription = velocity + " this is is height: " + heigth;
+    }
+}
+
+function intro(mario : Mario){
+    return "This is Mario and " + mario.velocity + " " + mario.heigth;
+}
+
+var player = new Mario(1, 1.50);
+
+console.log(intro(player));
+
 function gameLoop(ctx){
     requestAnimationFrame(gameLoop);
     ctx.fillStyle = "rgb(174,238,238)";
